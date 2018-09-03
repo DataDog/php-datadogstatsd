5da223aba6fa0740a11c29a98d3fc3996eb051d0
Farrukh Naeem <farrukh.naeem@outlook.com>
2018-09-03 17:49:02 +0500

    removed all the unnecessary code using protocols except UDP for sending stats

--- examples/example.php
+++ examples/example.php
@@ -1,21 +1,15 @@
 <?php
 
-require '../vendor/autoload.php';
+//Use autoload if installed with composer
+//require '../vendor/autoload.php'; Uncomment this if using composer
 
-use DataDog\DogStatsd;
-use DataDog\BatchedDogStatsd;
+//OR use direct path if not used composer
+require './src/CareStats.php';
 
-$statsd = new DogStatsd();
+use StatsDC\CareStats;
+
+$statsd = new CareStats();
 $statsd->increment('web.page_views');
 $statsd->histogram('web.render_time', 15);
 $statsd->distribution('web.render_time', 15);
-$statsd->set('web.uniques', 3 /* a unique user id */);
-$statsd->service_check('my.service.check', DogStatsd::CRITICAL);
-$statsd->event("Event title", array("text"=>"Event text"));
 
-//All the following metrics will be sent in a single UDP packet to the statsd server
-$batchedStatsd = new BatchedDogStatsd();
-$batchedStatsd->increment('web.page_views');
-$batchedStatsd->histogram('web.render_time', 15);
-$batchedStatsd->set('web.uniques', 3 /* a unique user id */);
-$batchedStatsd->flush_buffer(); // Necessary
