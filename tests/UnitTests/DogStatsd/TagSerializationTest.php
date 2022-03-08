80810bfa1dcce40bc6de536c3158e8fce07fc91e
Ben Roberts <beroberts@wayfair.com>
2022-03-08 15:56:11 -0500

    baseline passing tests

---
+++ tests/UnitTests/DogStatsd/TagSerializationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DataDog\UnitTests\DogStatsd;
+
+use PHPUnit\Framework\TestCase;
+use DataDog\DogStatsd;
+
+class TagSerializationTest extends TestCase
+{
+    private function callPrivate($object, $method, $params) {
+      $reflectionMethod = new \ReflectionMethod(get_class($object), $method);
+      $reflectionMethod->setAccessible(true);
+      return $reflectionMethod->invoke($object, $params);
+    }
+
+  /**
+   * @dataProvider tagProvider
+   *
+   * @param $tags
+   * @param $expected
+   */
+    public function testTagSerialization($tags, $expected) {
+      $dog = new DogStatsd();
+
+      $this->assertsame(
+          $expected,
+          $this->callPrivate($dog, 'serializeTags', $tags)
+      );
+    }
+
+    public function tagProvider()
+    {
+        return [
+            'without tags' => [
+                [],
+                ''
+            ],
+            'one string' => [
+                ['foo' => 'bar'],
+                '|#foo:bar'
+            ],
+            'two strings' => [
+                ['foo' => 'bar', 'baz' => 'blam'],
+                '|#foo:bar,baz:blam'
+            ],
+            'one string one int' => [
+                ['foo' => 'bar', 'baz' => 42],
+                '|#foo:bar,baz:42'
+            ]
+        ];
+    }
+}
