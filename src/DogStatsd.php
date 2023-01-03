bc69bfb92b2a6156c8bb5fd4acf5e85dad5e35b6
Brian Floersch <brian.floersch@datadoghq.com>
2023-01-03 13:31:00 -0500

    preparing release 0.5.6

--- src/DogStatsd.php
+++ src/DogStatsd.php
@@ -80,7 +80,7 @@ class DogStatsd
     private static $eventUrl = '/api/v1/events';
 
     // Used for the telemetry tags
-    public static $version = '1.5.5';
+    public static $version = '1.5.6';
 
     /**
      * DogStatsd constructor, takes a configuration array. The configuration can take any of the following values:
